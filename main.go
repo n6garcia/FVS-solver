940bf1860e92b0c9dac26bbb0952160307b4e717
Noel Garcia <n6garcia@ucsd.edu>
2022-08-08 22:23:52 -0700

    added member functions

--- main.go
+++ main.go
@@ -52,18 +52,43 @@ func (g *Graph) getVertex(k string) *Vertex {
 }
 
 func (g *Graph) deleteVertex(k string) {
-	delVertex := g.getVertex(k)
+	delIndex := g.getVertexIndex(k)
 
-	if delVertex == nil {
+	if delIndex == -1 {
 		fmt.Println("vertex doesn't exist!")
 	} else {
+		// delete vertex
+		g.vertices = remove(g.vertices, delIndex)
 
+		// delete edges
+		for _, v := range g.vertices {
+			for i, u := range v.adjacent {
+				if u.key == k {
+					v.adjacent = remove(v.adjacent, i)
+				}
+			}
+		}
 	}
+}
+
+func (g *Graph) getVertexIndex(k string) int {
+	for i, v := range g.vertices {
+		if v.key == k {
+			return i
+		}
+	}
+	return -1
+}
+
+func remove(s []*Vertex, i int) []*Vertex {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
 }
 
 func (g *Graph) Print() {
 	for _, v := range g.vertices {
 		fmt.Printf("\nVertex: %s", v.key)
+		fmt.Printf(" outEdges: ")
 		for _, v := range v.adjacent {
 			fmt.Printf(" %s ", v.key)
 		}
@@ -79,8 +104,14 @@ func main() {
 		tGraph.AddVertex(strconv.Itoa(i))
 	}
 
+	tGraph.AddEdge(strconv.Itoa(1), strconv.Itoa(3))
 	tGraph.AddEdge(strconv.Itoa(1), strconv.Itoa(2))
 	tGraph.AddEdge(strconv.Itoa(6), strconv.Itoa(2))
+	tGraph.AddEdge(strconv.Itoa(4), strconv.Itoa(3))
+
+	tGraph.deleteVertex(strconv.Itoa(0))
+	tGraph.deleteVertex(strconv.Itoa(6))
+	tGraph.deleteVertex(strconv.Itoa(2))
 
 	tGraph.Print()
 }
